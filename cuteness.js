6862b59ee54d1052419ffe1621af83401f25ddf8
root <root@mail.j-p.media>
2020-10-20 21:13:05 +0200

    added negative cuteness, modified roles

--- cuteness.js
+++ cuteness.js
@@ -4,12 +4,13 @@ class Bot{
     this.client = client;
     this.command = "!cute";
     this.altCommand = "!niedlich";
-    this.resetRoles = ["Moderator","CEO"];
+    this.resetRoles = ["Moderator","CEO","SIMP"];
     this.maxCuteness = 100;
     this.currentCuteness = 0;
-    this.barLength = 40;
+    this.barLength = 25;
     this.maxVotes = 3;
     this.currentVotes = [];
+    this.currentPerson = "-";
   }
 
   start(){
@@ -33,7 +34,7 @@ class Bot{
 	    } else this.currentVotes[msg.author.id] = 1;
 	    this.currentCuteness++;
 	    this.showCuteness(msg.channel);
-	  } else msg.reply("Skala schon voll. Frage doch einen Mod diese mit `!cute reset` zurückzusetzen");
+	  } else msg.reply("Skala schon voll. Frage doch einen Mod oder Simp diese mit `!cute reset` zurückzusetzen");
 	} else {
 	  switch (command[1]) {
 	    case "reset":
@@ -41,8 +42,11 @@ class Bot{
 		msg.reply("das kann nur ein mod machen...");
 		return;
 	      }
+	      if (command[2]) this.currentPerson = command[2];
+	      else command[2] = "-";
 	      this.currentVotes = [];
 	      this.currentCuteness = 0;
+	      if (this.currentPerson == "Julian" || this.currentPerson == "julian") this.currentCuteness = 500;
 	      this.showCuteness(msg.channel);
 
 	      break;
@@ -73,9 +77,18 @@ class Bot{
   }
 
   showCuteness(channel){
+    if (this.currentPerson == "Julian" || this.currentPerson == "julian") {
+      var str = `Level: ${this.currentCuteness}/${this.maxCuteness} Person: ${this.currentPerson}\n[${"=".repeat(this.barLength)}]------------------------------------------------||`; // hier minus eins weil der character in der mitte ja auch noch dazu kommt
+      channel.send(str);
+      return true;
+    }
+
     var currPos = this.currentCuteness / this.maxCuteness * this.barLength;
-    if (currPos < 0) currPos = 0;
-    var str = `[${"=".repeat(currPos)}||${"=".repeat(this.barLength - currPos - 1)}] Level: ${this.currentCuteness}/${this.maxCuteness}`; // hier minus eins weil der character in der mitte ja auch noch dazu kommt
+    var str;
+    if (currPos >= 0)
+    str = `Level: ${this.currentCuteness}/${this.maxCuteness} Person: ${this.currentPerson}\n[${"=".repeat(currPos)}||${"=".repeat(this.barLength - currPos - 1)}]`; // hier minus eins weil der character in der mitte ja auch noch dazu kommt
+    else
+    str = `Level: ${this.currentCuteness}/${this.maxCuteness} Person: ${this.currentPerson}\n||${"-".repeat(-currPos)}[${"=".repeat(this.barLength)}]`; // hier minus eins weil der character in der mitte ja auch noch dazu kommt
     channel.send(str);
     return true;
   }
@@ -86,7 +99,7 @@ class Bot{
           .setFooter("Cuteness-Bot von Julian Ullrich");
 
     reply.addFields({name:"Beschreibung",value:"Dieser Bot wurde - nach einer Idee von Darius -  geschrieben um die Cuteness verschiedener Profs zu tracken."},
-          {name:"Befehle",value:"```!cute -> erhöht die aktuelle cuteness um 1 und zeigt sie an.\n\n!cute - -> verringert die aktuelle cuteness um 1 und zeigt sie an.\n\n!cute show -> zeigt die aktuelle cuteness an.\n\n!cute reset -> setzt die aktuelle cuteness auf 0 zurück und zeigt sie an. Nur von Mods nutzbar.\n\n!cute help -> zeigt diese Hilfe an.```"});
+          {name:"Befehle",value:"```!cute -> erhöht die aktuelle cuteness um 1 und zeigt sie an.\n\n!cute - -> verringert die aktuelle cuteness um 1 und zeigt sie an.\n\n!cute show -> zeigt die aktuelle cuteness an.\n\n!cute reset name -> setzt die aktuelle cuteness auf 0 zurück, setzt den neuen namen und zeigt sie an. Nur von Mods und Simps nutzbar.\n\n!cute help -> zeigt diese Hilfe an.```"});
     reply.addFields({name:"Alias",value:"```!niedlich``` kann als Alias für ```!cute``` genutzt werden."});
 
         channel.send(reply);
